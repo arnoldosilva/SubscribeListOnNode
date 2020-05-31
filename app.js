921a70a83978cd7197b2ff1e8b85f7a5ef396c7c
Arnoldo Silva <analisi@gmail.com>
2020-05-30 21:16:37 -0300

    post init config

--- app.js
+++ app.js
@@ -7,11 +7,23 @@ const app = express()
 //! ERRO: Refused to apply style from 'http://localhost:3000/style.css' because its MIME 
 //! type ('text/html') is not a supported stylesheet MIME type, and strict MIME checking is enabled.
 
-app.use(express.static(__dirname));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}))
+
 
 app.get("/", function(req,res){
     res.sendFile(__dirname+"/signup.html")
 })
+
+app.post("/", function(req,res){
+    var firstName = req.body.fname
+    var lastName = req.body.lname
+    var email = req.body.email
+
+    console.log("Data " + firstName + " " + lastName + " " + email)
+
+})
+
 
 app.listen(3000,()=>{
     console.log("Ouvindo a porta 3000")
